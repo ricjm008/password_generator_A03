36331487a91f0d4619cbadb9fdda5371e4936cf2
Jared Rice <ricjm008@mymail.unisa.edu.au>
2021-03-22 18:54:56 +1030

    readme image

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ## Password Generator A03
 This assignment involves using javascript to write a random password generator. I altered some HTML and CSS as well, to increase the functionality of the website. Below is a screen shot of the deployed application with a link to the live website:
-[Screenshot](/images/.png)
+![Screenshot](/images/screenshot.png)
 [https://ricjm008.github.io/password_generator_A03/](https://ricjm008.github.io/password_generator_A03/)
--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var hasLowerCaseLetters = document.getElementById("#lcchar").checked;
-var hasUpperCaseLetters = document.getElementById("#ucchar").checked;
-var hasNumbers = document.getElementById("#nchar").checked;
-var hasSpecialCharacters = document.getElementById("#spchar").checked;
+var hasLowerCaseLetters = document.getElementById("lcchar").checked;
+var hasUpperCaseLetters = document.getElementById("ucchar").checked;
+var hasNumbers = document.getElementById("nchar").checked;
+var hasSpecialCharacters = document.getElementById("spchar").checked;
+let characters = "";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  
   var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
   var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   let passwordLength = 0;
  
-  passwordLength = document.getElementById("#pwleng").value;
-    
+  passwordLength = document.getElementById("pwleng").value;
+    console.log(passwordLength)
   if (passwordLength < 8 || passwordLength > 128) {
-      window.prompt("Password length must be 8-128 characters in length")
+      window.alert("Password length must be 8-128 characters in length")
   } 
-  var settings = {
-    passwordLength,
-    hasLowerCaseLetters,
-    hasUpperCaseLetters,
-    hasNumbers,
-    hasSpecialCharacters
+  console.log(hasLowerCaseLetters);
+  console.log(hasUpperCaseLetters);
+  console.log(hasNumbers);
+  console.log(hasSpecialCharacters);
+  console.log(lowerCaseLetters[15])
+  if (!hasLowerCaseLetters && !hasNumbers && !hasSpecialCharacters && !hasUpperCaseLetters) {
+    return;
   }
   if (hasLowerCaseLetters) {
     characters += lowerCaseLetters
@@ -40,15 +38,21 @@ function writePassword() {
   } if (hasSpecialCharacters) {
     characters += specialCharacters
   }
+  console.log(characters);
 
- generatePassword = function (characters) {
+ generatePassword = function () {
     var charSetLength = characters.length;
-    var results = "";
+    var password = "";
     for (let index = 0; index < passwordLength; index++) {
-      results += characters[Math.floor(Math.random()*charSetLength)];
-      return results;
+      password += characters[Math.floor(Math.random()*charSetLength)];
+      ;
     }
+    return password;
   }
+password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 // TODO: add ifs for passwordLength, lowerCaseLetters, upperCaseLetters, numbers, specialCharacters
 }
